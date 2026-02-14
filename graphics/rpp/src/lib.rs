8108494f505fdb12e65ec859be9c2831030f794f
Hasegawa, Raku <raku@exhibition.jp>
2026-02-14 20:10:17 +0900

    rpp: Update wgpu version

--- graphics/rpp/src/lib.rs
+++ graphics/rpp/src/lib.rs
@@ -1,9 +1,11 @@
 pub mod context;
 pub mod geometry;
 pub mod shader;
+pub mod tree;
 #[cfg(feature = "wgpu")]
 pub mod wgpu;
 
 pub use context::*;
 pub use geometry::*;
 pub use shader::*;
+pub use tree::*;
