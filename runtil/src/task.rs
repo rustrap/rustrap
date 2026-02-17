9333261b840a9d4b8e643d7b5b4f6cac46878bef
Hasegawa, Raku <raku@exhibition.jp>
2026-02-17 20:22:02 +0900

    runtil: Remove unused symbols

--- runtil/src/task.rs
+++ runtil/src/task.rs
@@ -1,20 +1,4 @@
-use std::sync::Mutex;
-
-use futures::future::BoxFuture;
-
 use crate::actor::MainMarker;
-
-pub struct AsyncTask {
-    fut: Mutex<Option<BoxFuture<'static, ()>>>,
-}
-
-impl AsyncTask {
-    pub fn new(fut: BoxFuture<'static, ()>) -> Self {
-        AsyncTask {
-            fut: Mutex::new(Some(fut)),
-        }
-    }
-}
 
 pub struct MainTask {
     pub(crate) f: Box<dyn Fn(MainMarker) -> ()>,
